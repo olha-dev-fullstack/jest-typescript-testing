1ce1af3b165c22d1f88f41e244f412413108535a
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-05-01 12:39:36 +0300

    refactor and add coverage

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,12 @@
+export class StringUtils {
+  public toUpperCase(arg: string) {
+    if (!arg) {
+      throw new Error("Invalid argument!");
+    }
+    return toUpperCase(arg);
+  }
+}
+
 export function toUpperCase(arg: string) {
   return arg.toUpperCase();
 }
@@ -10,6 +19,7 @@ export type stringInfo = {
   extraInfo: Object | undefined;
 };
 
+/* istanbul ignore next */
 export function getStringInfo(arg: string): stringInfo {
   return {
     lowerCase: arg.toLowerCase(),
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,6 +1,44 @@
-import { getStringInfo, toUpperCase } from "../app/Utils";
+import { getStringInfo, StringUtils, toUpperCase } from "../app/Utils";
 
 describe("Utils test suite", () => {
+  describe.only("StringUtils tests", () => {
+    let sut: StringUtils;
+
+    beforeEach(() => {
+      sut = new StringUtils();
+    });
+
+    it("Should return correct upperCase", () => {
+      const actual = sut.toUpperCase("abc");
+      expect(actual).toBe("ABC");
+    });
+
+    it("Should throw error on invalid argument - function", () => {
+      function expectError() {
+        const actual = sut.toUpperCase("");
+      }
+      expect(expectError).toThrow();
+      expect(expectError).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - arrow function", () => {
+      expect(() => {
+        sut.toUpperCase("");
+      }).toThrowError("Invalid argument!");
+    });
+
+    it("Should throw error on invalid argument - try catch block", (done) => {
+      try {
+        sut.toUpperCase("");
+        done("GetStringInfo should throw error for invalid arg!");
+      } catch (error) {
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toHaveProperty("message", "Invalid argument!");
+        done();
+      }
+    });
+  });
+
   it("should return uppercase of valid string", () => {
     const sut = toUpperCase;
     const expected = "ABC";
