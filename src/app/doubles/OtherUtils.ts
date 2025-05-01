d74c02f8d8ad9a97396c635158d7694f01dc3909
olha-dev-fullstack <olha.shevel.dev@gmail.com>
2025-05-01 14:28:55 +0300

    add test mocks

---
+++ src/app/doubles/OtherUtils.ts
@@ -0,0 +1,23 @@
+export type stringInfo = {
+    lowerCase: string;
+    upperCase: string;
+    characters: string[];
+    length: number;
+    extraInfo: Object | undefined;
+  };
+
+  type LoggerServiceCallback = (arg: string) => void;
+  
+  export function calculateComplexity(stringInfo: stringInfo) {
+    return Object.keys(stringInfo.extraInfo).length * stringInfo.length
+  }
+
+  export function toUpperCaseWithCb(arg: string, callBack: Function) {
+    if(!arg) {
+        callBack('Invalid argument!');
+        return;
+    }
+
+    callBack(`called function with ${arg}`)
+    return arg.toUpperCase()
+  }
